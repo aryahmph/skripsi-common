38584d15b6f0710ae906d149a0c6239d5e729f7e
Arya Yunanta <aryayun90@gmail.com>
2023-08-16 11:46:41 +0700

    update

--- http/type.go
+++ http/type.go
@@ -1,7 +1,30 @@
 package http
 
+import "time"
+
 type (
 	Response struct {
 		Data any `json:"data"`
+	}
+
+	User struct {
+		ID           string    `json:"id"`
+		Name         string    `json:"name"`
+		Email        string    `json:"email"`
+		PasswordHash string    `json:"password_hash"`
+		Role         string    `json:"role"`
+		CreatedAt    time.Time `json:"created_at"`
+		UpdatedAt    time.Time `json:"updated_at"`
+	}
+
+	AddUser struct {
+		Name     string `json:"name" validate:"required,lte=255"`
+		Email    string `json:"email" validate:"required,email"`
+		Password string `json:"password" validate:"required,gte=6,lte=16"`
+	}
+
+	Login struct {
+		Email    string `json:"email" validate:"required,email"`
+		Password string `json:"password" validate:"required,gte=6,lte=16"`
 	}
 )
